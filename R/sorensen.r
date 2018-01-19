27b40248b965b761e287a25adcb1aad79c545584
Matthew Talluto <mtalluto@gmail.com>
2018-01-19 16:05:12 +0100

    Implemented partitioning of sorensen diversity as well as PD and FD for sorensen

--- R/sorensen.r
+++ R/sorensen.r
@@ -1,57 +1,169 @@
 #' Sorensen dissimilarity index
 #' 
 #' @param comm Community matrix giving sites (rows) by species (columns).
-#' @param dist Optional species by species distance matrix; margin names must match colnames in comm.
-#' @references Pavoine, S. and Ricotta, C. 2014. Functional and phylogenetic similarity among communities.
-#' Methods in Ecology and Evolution 5(7): 666–675.
-#' @return Matrix of pairwise dissimilarity values between communities.
+#' @param partition Logical; if true, the index will be partitioned into turnover and
+#' 			nestedness.
+#' @references Baselga A. 2010. Partitioning the turnover and nestedness components of 
+#' 		beta diversity. Global Ecol. Biogeogr. 19: 134–143
+#' @references Sørensen, T. (1948) A method of establishing groups of equal amplitude in 
+#' 		plant sociology based on similarity of species content. Kongelige Danske 
+#' 		Videnskabernes Selskabs Biologiske Skrifter, 5, 1–34.
+#' @return If \code{partition == TRUE}, list of three matrices giving the turnover,
+#' 		nestedness, and total components of dissimilarity. Otherwise, a matrix of
+#' 		pairwise dissimilarity values between communities.
 #' @export
-sorensen <- function(comm, dis)
-{
-	if(missing(dis))
-		return(sor(comm))
-
-	if((nrow(dis) != ncol(dis)) | any((colnames(dis) != rownames(dis))))
-		stop("dis must be square with identical row and column names")
-	
-	taxa <- sort(intersect(colnames(comm), rownames(dis)))
-	if(length(taxa) < ncol(comm)) 
-		warning(ncol(comm) - length(taxa), " taxa were dropped from comm because they were not in dis")
-	if(length(taxa) < nrow(dis))
-		warning(nrow(dis) - length(taxa), " taxa were dropped from dis because they were not in comm")
-	dis <- dis[taxa, taxa]
-	comm <- comm[,taxa]
-
-	sim <- 1 - dis
-	
-	num <- matrix(nrow = nrow(comm), ncol=nrow(comm))
-	for(a in 1:nrow(comm)) {
-		for(b in a:nrow(comm)) {
-			num[a,b] <- sum(outer(comm[a,], comm[b,]) * sim)
-		}
-	}
-	num[lower.tri(num)] <- t(num)[lower.tri(num)]
-	denom <- outer(0.5*diag(num), 0.5*diag(num), '+')
-	res <- 1 - num/denom
-	rownames(res) <- colnames(res) <- rownames(comm)
-	res
-
-}
-
-
-#' Sorensen dissimilarity (without distance)
-#' 
-#' Compute original sorensen dissimilarity index, with no option for a distance matrix among species
-#' 
-#' @param comm Community matrix giving sites (rows) by species (columns).
-#' @details The index is defined as 1 - (2 * (A ∩ B) / (A + B)). 
-#' @references Sørensen, T. (1948) A method of establishing groups of equal amplitude in plant sociology 
-#' based on similarity of species content. Kongelige Danske Videnskabernes Selskabs Biologiske Skrifter, 5, 1–34.
-#' @return Matrix of pairwise dissimilarity values between communities.
-sor <- function(comm)
+sorensen <- function(comm, partition = FALSE)
 {
 	if(!is.matrix(comm)) comm <- as.matrix(comm)
 
+	a <- comm %*% t(comm > 0)
+	b <- comm %*% t(!comm)
+	cc <- t(b)
+	bsor <- (b + cc) / (2*a + b + cc)
+
+	if(partition) {
+		mbc <- pmin(b, cc)
+		bturn <- mbc / (a + mbc)
+		bnes <- bsor - bturn
+		return(list(turnover = bturn, nestedness = bnes, sorensen = bsor))
+	} else
+		return(bsor)
+}
+
+#' Phylogenetic sorensen dissimilarity index
+#' 
+#' @param comm Community matrix giving sites (rows) by species (columns).
+#' @param partition Logical; if true, the index will be partitioned into turnover and
+#' 			nestedness.
+#' @references Pavoine, S. and Ricotta, C. 2014. Functional and phylogenetic similarity 
+#' 		among communities. Methods in Ecology and Evolution 5(7): 666–675.
+#' @return If \code{partition == TRUE}, list of three matrices giving the turnover,
+#' 		nestedness, and total components of dissimilarity. Otherwise, a matrix of
+#' 		pairwise dissimilarity values between communities.
+#' @export
+phylosor <- function(comm, tree, partition = FALSE)
+{
+	if(is.null(tree$edge.length))
+		stop("The tree must have branch lengths")
+
+	if(any(! comm %in% c(0,1) )) {
+		warning("Community matrix can only contain 0 and 1; assigning 1 to all nonzero 
+			entries")
+		comm <- 1 * (comm > 0)
+	}
+
+	# node labels and community columns must match
+	commDrop <- which(!colnames(comm) %in% tree$tip.label)
+	if(length(commDrop) > 0)
+	{
+		if(length(commDrop) == ncol(comm))
+			stop("No columns in comm match tip labels in tree")
+
+		warning("Dropping ", length(commDrop), " columns from comm that were 
+			not found in tree")
+
+		comm <- comm[,-commDrop]
+	}
+	treeDrop <- which(!tree$tip.label %in% colnames(comm))
+	if(length(treeDrop) > 0)
+	{
+		if(length(treeDrop) == length(tree$tip.labe))
+			stop("No tip labels in tree were found in column labels in comm")
+
+		warning("Dropping ", length(treeDrop), " modes from tree that were 
+			not found in comm")
+
+		tree <- ape::drop.tip(tree, treeDrop)
+	}
+
+	if(any(rowSums(comm) == 0))
+	{
+		drop <- which(rowSums(comm) == 0)
+		warning(length(drop), " sites had 0 species and were dropped")
+		comm <- comm[-drop,]
+	}
+
+	branch_sp <- branch_adjacency(tree, TRUE)
+	# save weights
+	wts <- attr(branch_sp, "branch.lengths")
+	# match the margins
+	branch_sp <- branch_sp[,match(colnames(comm), colnames(branch_sp))]
+
+	site_branch <- comm %*% t(branch_sp)
+	# convert site by branch to presence absence (instead of sp count)
+	site_branch <- 1 * (site_branch > 0)
+	# now weight by branch lengths
+	site_branch <- sweep(site_branch, 2, wts, `*`)
+	sorensen(site_branch, partition)
+}
+
+
+#' Sorensen dissimilarity index based on a dendrogram
+#' 
+#' Produces dissimilarity matrices using a species by species distance matrix; useful
+#' for example for functional diversity.
+#' 
+#' @param comm Community matrix giving sites (rows) by species (columns).
+#' @param distmat Distance matrix; margins must match exactly the columns in comm 
+#' @references Pavoine, S. and Ricotta, C. 2014. Functional and phylogenetic similarity 
+#' 		among communities. Methods in Ecology and Evolution 5(7): 666–675.
+#' @return If \code{partition == TRUE}, list of three matrices giving the turnover,
+#' 		nestedness, and total components of dissimilarity. Otherwise, a matrix of
+#' 		pairwise dissimilarity values between communities.
+#' @export
+dendrosor <- function(comm, distmat, partition = TRUE)
+{
+	if(!inherits(distmat, "dist"))
+		distmat <- as.dist(distmat)
+
+	if(!attr(distmat, "Size") == ncol(comm) & 
+		any(attr(distmat, "Labels") != colnames(comm)))
+		stop("ncol(comm) must equal the number of rows/columns in distmat and the
+			margin labels must be identical")
+
+	tree <- hclust(distmat)
+	list(sorensen = phylosor(comm, ape::as.phylo(tree), partition), dendrogram = tree)
+}
+
+#' Branch adjacency
+#' 
+#' Produce a branch adjacency matrix (i.e., branch by node)
+#' 
+#' @param x Object of class phylo
+#' @param branch.lengths Logical; if true, the values of the adjacency matrix will be the
+#' 		branch lengths, otherwise they will be 1 or 0
+#' @return A branch by node adjacency matrix
+#' @keywords internal
+branch_adjacency <- function(x, branch.lengths = !is.null(x$edge.length)) {
+    m <- matrix(0, ncol=length(x$tip), nrow=nrow(x$edge))
+    from <- x$edge[,1]
+    to <- x$edge[,2]
+    g <- seq_along(x$tip.label)
+    while (any(!is.na(g))) {
+        i <- match(g, to)
+        m[cbind(i, seq_along(i))] <- 1
+        g <- from[i]
+    }
+    rownames(m) <- paste0("Branch", seq.int(nrow(m)))
+    colnames(m) <- x$tip.label
+    if(branch.lengths)
+    {
+    	attr(m, "branch.lengths") <- x$edge.length
+    }
+    return(m)
+}
+
+
+#' Sorensen dissimilarity (without partitioning)
+#' 
+#' Compute sorensen dissimilarity index; deprecated method (but faster than new one)
+#' 
+#' @param comm Community matrix giving sites (rows) by species (columns).
+#' @details The index is defined as 1 - (2 * (A ∩ B) / (A + B)). 
+#' @return Matrix of pairwise dissimilarity values between communities.
+#' @keywords internal
+sor <- function(comm)
+{
 	# convert to matrix of 0 and 1	
 	if(any(! comm %in% c(0,1) )) comm <- 1 * (comm > 0)
 	# get the intersection of all sites - number of sp in common
